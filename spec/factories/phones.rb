f3df7481f9d89a5987a6ffebfd7dc3626b9c9e1d
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-04 23:40:44 -0800

    refactor phone factory to utilize nested factory

--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -1,7 +1,16 @@
 FactoryGirl.define do
   factory :phone do  
     association :contact #tells factory girl to create a contact on the fly for this phone to belong to
-    phone '123-555-1234'
-    phone_type 'home'
+    phone {'123-555-1234'}
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+    factory :work_phone do
+      phone_type 'work'
+    end
+    factory :mobile_phone do
+      phone_type 'mobile'
+    end
   end
 end
