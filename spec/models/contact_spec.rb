0fbab5d0584409ac87c2ef35da0fd5a49e3b76dc
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-04 22:52:00 -0800

    refactor Contact tests to utilize factory girl

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Contact do
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:contact)).to be_valid
+  end
+
   it "is valid with a firstname, lastname and email" do 
     contact = Contact.new(
       firstname: 'Faraaz',
@@ -10,37 +14,36 @@ describe Contact do
   end
 
   it "is invalid without a firstname" do
-    contact = Contact.new(firstname: nil)
+    contact = FactoryGirl.build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   it "is invalid without a lastname" do
-    contact = Contact.new(lastname: nil)
+    contact = FactoryGirl.build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
   it "is invalid without an email address" do
-    contact = Contact.new(email: nil)
+    contact = FactoryGirl.build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
 
-  it "is invalid with a duplicate email address" do 
-    Contact.create(firstname: 'vince', lastname: 'perez', email: 'fnishtar@apple.com')
-    contact = Contact.new(firstname: 'vince', lastname: 'perez', email: 'fnishtar@apple.com')
-    
-    contact.valid?
-    expect(contact.errors[:email]).to include("has already been taken")
-  end
-
   it "returns a contact's full name as a string" do
-    contact = Contact.new(
+    contact = FactoryGirl.build(:contact,
       firstname: 'Faraaz',
       lastname: 'Nishtar',
       email: 'faraaznishtar@gmail.com')
     expect(contact.name).to be_a(String)
+  end
+
+  it "is invalid with a duplicate email address" do 
+    FactoryGirl.create(:contact, email: 'fnishtar@apple.com')
+    contact = FactoryGirl.build(:contact, email: 'fnishtar@apple.com')
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
   end
 
   it "returns the contact's first and last name" do 
