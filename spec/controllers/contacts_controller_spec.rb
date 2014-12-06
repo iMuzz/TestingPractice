08d5489b502ea5e4db46a9dac0abb4ab79284262
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-06 00:00:28 -0800

    create tests for when there is no user logged in -m new, edit, create, update, and destroy have been created

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -384,6 +384,40 @@ describe ContactsController do
       end
     end
 
-    
+    describe 'GET #new' do
+      it "requires login" do
+        get :new
+        expect(response).to redirect_to login_url
+      end
+    end
+
+    describe 'GET #edit' do
+      it "requires login" do
+        contact = create(:contact)
+        get :edit, id: contact
+        expect(response).to redirect_to login_url
+      end
+    end
+
+    describe 'POST #create' do
+      it "requires login" do
+        post :create, id: create(:contact), contact: attributes_for(:contact)
+        expect(response).to redirect_to login_url
+      end
+    end
+
+    describe 'PUT #update' do
+      it "requires login" do 
+        put :update, id: create(:contact), contact: attributes_for(:contact)
+        expect(response).to redirect_to login_url
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it "requires login" do
+        delete :destroy, id: create(:contact)
+        expect(response).to redirect_to login_url
+      end
+    end
   end
 end
