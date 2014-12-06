571dd87e21d25e3cc7d84bdb4fdc1c2cb80ecb3c
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-05 21:27:40 -0800

    implement POST #create tests

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -70,19 +70,39 @@ describe ContactsController do
     it "renders the :edit template" do
       contact = create(:contact)
       get :edit, id: contact
-      expect(response). to render_template :edit
+      expect(response).to render_template :edit
     end
   end
 
   describe 'POST #create' do 
+    before :each do 
+      @phones = [attributes_for(:phone), attributes_for(:phone), attributes_for(:phone)]
+    end
     context "with valid attributes" do
-      it "savesthe new contact in the database"
-      it "redirects to contacts#show"
+      it "saves the new contact in the database" do
+        expect {
+          post :create, contact: attributes_for(:contact, phones_attributes: @phones)
+        }.to change(Contact, :count).by(1)
+      end
+
+      it "redirects to contacts#show" do
+        post :create, contact: attributes_for(:contact, phones_attributes: @phones)
+        expect(response).to redirect_to contact_path(assigns[:contact])
+      end
     end
 
     context "with invalid attributes" do
-      it "does not save the new contact in the database"
-      it "re-renders the :new template"
+      it "does not save the new contact in the database" do
+        expect {
+          post :create, contact: attributes_for(:invalid_contact,
+            phones_attributes: @phones)
+        }.to_not change(Contact, :count)
+      end
+
+      it "re-renders the :new template" do
+        post :create, contact: attributes_for(:invalid_contact, phones_attributes: @phones)
+        expect(response).to render_template :new
+      end
     end
   end 
 
