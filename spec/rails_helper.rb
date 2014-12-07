62dc22885193ae08418861bc646288c433df6029
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-06 21:13:50 -0800

    let RSpec know to include LoginMacros module and support directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -26,7 +27,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  # config.include LoginMacros #saying uninitialized constant, (Think it has to do with gitignore)
+  config.include LoginMacros
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 end
