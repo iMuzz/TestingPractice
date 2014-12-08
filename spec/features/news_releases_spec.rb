c80d1291fc01b3d8590a84581f86df996c90e528
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-12-07 18:15:37 -0800

    create outline test for news releases feature

---
+++ spec/features/news_releases_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "News releases" do
+  context "as a user" do 
+    scenario "adds a new user release"
+  end
+
+  context "as a guest" do
+    scenario "reads a news release"
+  end
+end
